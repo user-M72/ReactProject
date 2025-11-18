d0a1e2b641a7f9b305a3dfb04e17d6f0e1b3393f
LinKord Azamov <muzaffarazamov007@gmail.com>
2025-11-19 02:46:46 +0600

    Added home page and updated register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import { useState } from 'react'
 import './App.css'
-import RegisterForm from './components/RegisterForm'
+import HomePage from './pages/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div className="card">
-        <h1>Task Hub Application</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-
-      {/* Форма регистрации */}
-      <RegisterForm />
-    </>
-  )
+  return <HomePage />
 }
 
 export default App
